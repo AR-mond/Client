2d93b5cc4df661d6dfc742bb8ca3b057f25aee00
dongree <dongwoo0307@naver.com>
2022-10-02 22:17:19 +0900

    update home footer

--- src/components/home_footer/home_footer.jsx
+++ src/components/home_footer/home_footer.jsx
@@ -4,12 +4,24 @@ import styles from './home_footer.module.css';
 const HomeFooter = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer_text}>상호 : (주)000 | 대표자명 : 000 </div>
-      <div className={styles.footer_text}>사업자등록번호 : 000-00-000000</div>
-      <div className={styles.footer_text}>연락처 : 000-0000-0000</div>
-      <div className={styles.footer_text}>
-        이메일 : 00000@0000.com 주소: 00시 000 000 0000 00
-      </div>
+      <ul className={styles.menu}>
+        <li className={styles.menu_item}>COMPANY</li>
+        <li className={styles.menu_item}>SERVICES</li>
+        <li className={styles.menu_item}>ABOUT</li>
+        <li className={styles.menu_item}>REVIEWS</li>
+      </ul>
+      <ul className={styles.info}>
+        <li className={styles.info_item}>상호 : (주)000 | 대표자명 : 000 </li>
+        <li className={styles.info_item}>사업자등록번호 : 000-00-000000</li>
+        <li className={styles.info_item}>연락처 : 000-0000-0000</li>
+        <li className={styles.info_item}>
+          이메일 : 00000@0000.com 주소: 00시 000 000 0000 00
+        </li>
+      </ul>
+      <ul className={styles.terms}>
+        <li className={styles.terms_item}>이용약관</li>
+        <li className={styles.terms_item}>개인정보처리방침</li>
+      </ul>
     </footer>
   );
 };
