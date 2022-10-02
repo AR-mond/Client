8669d3aa2f0e6ab8550056016ce208be563859a9
dongree <dongwoo0307@naver.com>
2022-10-02 20:28:09 +0900

    update home main

--- src/components/home_main/home_main.jsx
+++ src/components/home_main/home_main.jsx
@@ -5,14 +5,22 @@ const HomeMain = () => {
   return (
     <section className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.title}>설계 모델 증강현실 자동 견적 서비스</h1>
-        <p className={styles.info}>
-          설계 모델 자동 분석과 증강현실 프리뷰를 하이쓰리디에서 한번에
-          해결하세요.
+        <p className={styles.p1}>
+          3D 모델 AR프리뷰와
+          <br /> 설계제작까지
+        </p>
+        <p className={styles.p2}>
+          아몬드 <span className={styles.highlight}>ArToArt</span>에서
+        </p>
+        <p className={styles.p3}>
+          완성도 높고 신뢰할 만한 품질의 <br />
+          설계 모델 자동 분석, 증강현실 프리뷰와
+          <br />
+          실제 제작을 아몬드에서 해결하세요
         </p>
         <div className={styles.startBtn}>
-          <span className={styles.btnTitle}>지금 시작하기</span>
-          <img src="" alt="" />
+          <span className={styles.btnTitle}>바로가기</span>
+          <img src="icon/next.svg" />
         </div>
       </div>
     </section>
