1947879ad6df841e3b8401742c09bd91245dfb2b
dongree <dongwoo0307@naver.com>
2023-05-14 14:10:05 +0900

    plantviewer css 수정

--- src/components/plantviewer/plantviewer.jsx
+++ src/components/plantviewer/plantviewer.jsx
@@ -8,44 +8,45 @@ function PlantViewer() {
   useEffect(() => {
     const modelViewerVariants = modelViewerVariantsRef.current;
     const select = selectRef.current;
-  
-    modelViewerVariants.addEventListener("load", () => {
+
+    modelViewerVariants.addEventListener('load', () => {
       const names = modelViewerVariants.availableVariants;
       select.innerHTML = '';
       for (const name of names) {
-        const option = document.createElement("option");
+        const option = document.createElement('option');
         option.value = name;
         option.textContent = name;
         select.appendChild(option);
       }
     });
-  
-    select.addEventListener("input", (event) => {
+
+    select.addEventListener('input', event => {
       modelViewerVariants.variantName =
-        event.target.value === "default" ? null : event.target.value;
+        event.target.value === 'default' ? null : event.target.value;
     });
   }, []);
-  
 
   return (
     <section className={styles.main}>
-    <model-viewer
-      ref={modelViewerVariantsRef}
-      src="3d/plant.glb"
-      camera-controls
-      ar
-      ar-modes="webxr scene-viewer quick-look"
-      shadow-intensity="2"
-      shadow-softness="0"
-      autoplay
-    >
-      <div className={styles.controls}>
-        <div>
-          <h3>화분의 소재를 변경 해보세요~</h3>
-          <select ref={selectRef}></select>
-        </div>
+      <div className={styles.modelViewer}>
+        <model-viewer
+          ref={modelViewerVariantsRef}
+          src="3d/plant.glb"
+          camera-controls
+          ar
+          ar-modes="webxr scene-viewer quick-look"
+          shadow-intensity="2"
+          shadow-softness="0"
+          autoplay
+        >
+          <div className={styles.controls}>
+            <div>
+              <h3>화분의 소재를 변경 해보세요~</h3>
+              <select ref={selectRef}></select>
+            </div>
+          </div>
+        </model-viewer>
       </div>
-    </model-viewer>
     </section>
   );
 }
